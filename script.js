8cccf35d9c2ce804b6726adde8da2f0143e019b0
Ella Meek <ellameek2028@u.northwestern.edu>
2026-09-14 14:30:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const bios = {
     role: "Project Manager - EEG Wheelchair Team",
     text: "Hey all, my name is Luca Endorf and I am a junior at NU. I’m a Mechanical engineering student with a concentration in robotics, and in my free time I enjoy reading, going to the gym, hanging with friends, and listening to/playing music. Reach out if you have questions about the EEG team!",
     image: "images/luca_endorf.jpg",
-    // linkedin: "",
+    linkedin: "https://www.linkedin.com/in/luca-endorf-391053359/",
     email: "mailto:LucaEndorf2028@u.northwestern.edu"
   },
   dev: {
@@ -41,8 +41,8 @@ const bios = {
     role: "Secretary",
     text: "Hi, my name is Dev Jhaveri and I’m from Mumbai, India. I am a junior studying ME and in my free time I like to play table tennis and go food exploring.",
     image: "images/dev_jhaveri.jpeg",
-  //   linkedin: "https://www.linkedin.com/in/devjhaveri/",
-  email: "mailto:DevJhaveri2028@u.northwestern.edu"
+    linkedin: "https://www.linkedin.com/in/devjhaveri/",
+    email: "mailto:DevJhaveri2028@u.northwestern.edu"
   },
   meghna: {
     name: "Meghna Sundaram",
