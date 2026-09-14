47b339e22df3001d95e62d27fb3234f428f412a0
Ella Meek <ellameek2028@u.northwestern.edu>
2026-09-14 14:04:00 -0400

    Updates headshots + bios

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const bios = {
-  nikhil: {
-    name: "Nikhil Nayak",
-    role: "Co-President",
-    text: "Hey guys! My name is Nikhil and I'm a senior at NU this year. I'm in the BS/MS program studying mechanical and electrical engineering, and outside of school I like to listen to new songs, hang out with friends, and spend time outside. Feel free to reach out if you have any questions!",
-    image: "images/nik.jpg",
-    linkedin: "https://www.linkedin.com/in/n-nayak1/",
-    email: "mailto:NikhilNayak2026@u.northwestern.edu"
-  },
   nathaniel: {
     name: "Nathaniel Rogers",
-    role: "Co-President",
-    text: "Hello! I’m Nathaniel, and I’m a junior at NU pursuing a degree in Biomedical Engineering and the Segal Design Certificate. I’m especially interested in medical devices and the design engineering behind them. Extracurricularly, I enjoy playing squash, reading, and long, exploratory walks. Please stop me on a path or send an email/message if you have questions or comments about Medical Makers, BME, or anything else I mentioned above.",
+    role: "Project Manager – EEG Wheelchair Team",
+    text: "Hi! I’m Nathaniel, and I’m a senior studying Biomedical Engineering and product design through the Segal Design Certificate. I’m deeply interested in all flavors of medical device product design, development, and engineering, though much of my experience falls in the cardiac electrophysiology world. Extracurricularly, I enjoy playing squash, reading, and long walks. I’m also a member of the NU cheerleading team! Please feel free to reach out with any inquiries.",
     image: "images/nathaniel_rogers.jpeg",
     linkedin: "https://www.linkedin.com/in/nathaniel-r0gers/",
     email: "mailto:NathanielRogers2027@u.northwestern.edu"
@@ -30,56 +22,48 @@ const bios = {
   },
   jackson: {
     name: "Jackson Wang",
-    role: "Secretary",
-    text: "Hi, my name is Jackson Wang and I’m from Nashville TN. I am a sophomore studying BME and in my free time I like to do photography and go hiking.",
+    role: "Project Manager - Team 2",
+    text: "Hi, my name is Jackson Wang and I’m from Nashville TN. I am a Junior studying BME and in my free time I like to do photography and go hiking.",
     image: "images/jackson_wang.jpeg",
     linkedin: "https://www.linkedin.com/in/jackson-wang-899a25172",
     email: "mailto:JacksonWang2028@u.northwestern.edu"
   },
-  victoria: {
-    name: "Victoria Israel",
-    role: "Project Manager - MILO",
-    text: "Hey everyone! I'm a senior in the BS/MS program, majoring in Mechanical Engineering. I'm passionate about building solutions and tackling new challenges. I'm always happy to share advice, whether it's about majors, clubs, or life in general, so don't hesitate to reach out and send me an email!",
-    image: "images/victoria_israel.jpeg",
-    linkedin: "https://www.linkedin.com/in/victoriaisrael",
-    email: "mailto:VictoriaIsrael2026@u.northwestern.edu"
+  luca: {
+    name: "Luca Endorf",
+    role: "Project Manager - EEG Wheelchair Team",
+    text: "Hey all, my name is Luca Endorf and I am a junior at NU. I’m a Mechanical engineering student with a concentration in robotics, and in my free time I enjoy reading, going to the gym, hanging with friends, and listening to/playing music. Reach out if you have questions about the EEG team!",
+    image: "images/luca_endorf.jpg",
+    // linkedin: "",
+    email: "mailto:LucaEndorf2028@u.northwestern.edu"
   },
-  eva: {
-    name: "Eva Potjer",
-    role: "Project Manager - SlideBoard",
-    text: "Hi! I'm Eva and I'm a senior at Northwestern studying Biomedical Engineering and Neuroscience!",
-    image: "images/eva_potjer.jpeg",
-    linkedin: "https://www.linkedin.com/in/eva-potjer-617598252",
-    email: "mailto:EvaPotjer2026@u.northwestern.edu"
-  },
+  dev: {
+    name: "Dev Jhaveri",
+    role: "Secretary",
+    text: "Hi, my name is Dev Jhaveri and I’m from Mumbai, India. I am a junior studying ME and in my free time I like to play table tennis and go food exploring.",
+    image: "images/dev_jhaveri.jpeg",
+  //   linkedin: "https://www.linkedin.com/in/eva-potjer-617598252",
+  email: "mailto:DevJhaveri2028@u.northwestern.edu"
+  // },
   meghna: {
     name: "Meghna Sundaram",
-    role: "Project Manager - SlideBoard",
-    text: "Junior studying Mechanical Engineering. She is specifically interested in design and CAD modeling for robotics and biomedical applications. For fun, she loves to bake, go on long runs and hang out with her friends.",
+    role: "Project Manager - Team 2",
+    text: "Senior studying Mechanical Engineering. She is specifically interested in design and CAD modeling for robotics and biomedical applications. For fun, she loves to bake, go on long runs and hang out with her friends.",
     image: "images/meghna_sundaram.jpeg",
     linkedin: "https://www.linkedin.com/in/meghna-sundaram-32973b283",
     email: "mailto:MeghnaSundaram2027@u.northwestern.edu"
   },
   travis: {
     name: "Travis Saltzman",
-    role: "Project Manager - Team 1",
-    text: "Hey! I'm Travis and I'm from Denver, Colorado. I am a Junior that transferred to NU last year majoring in Biomedical Engineering with a pre-medical designation. I'm passionate about work involving medical devices and biosensors, with a specific interest in combating neurological conditions. In my free time, I like to workout, play basketball, or just hang out with friends. If you have any questions, feel free to reach out!",
+    role: "Co-President",
+    text: "Hey! I'm Travis and I'm from Denver, Colorado. I am a Senior majoring in Biomedical Engineering with a pre-medical designation. I'm passionate about work involving medical devices and biosensors, with a specific interest in combating neurological conditions. In my free time, I like to workout, play basketball, or just hang out with friends. If you have any questions, feel free to reach out!",
     image: "images/travis_saltzman.jpeg",
     linkedin: "https://www.linkedin.com/in/travis-saltzman-9a0b7225a",
     email: "mailto:TravisSaltzman2027@u.northwestern.edu"
   },
-  david: {
-    name: "David Wong",
-    role: "Project Manager - Team 2",
-    text: "Hi! I'm David and I'm a junior at Northwestern studying Biomedical Engineering and Computer Science!",
-    image: "images/david_wong.jpeg",
-    linkedin: "https://www.linkedin.com/in/david-c-w-nu2027/",
-    email: "mailto:DavidWong2027@u.northwestern.edu"
-  },
   ella: {
     name: "Ella Meek",
-    role: "Project Manager - Team 2",
-    text: "Hi, I’m Ella and I’m from South Florida. I’m a Sophomore studying biomedical engineering with an interest in bionic prosthetic limbs. My favorite thing about Medical Makers is that our projects bridge engineering and patient care. Outside of school, I enjoy playing volleyball, hanging out with friends, and trying new places to eat!",
+    role: "Co-President",
+    text: "Hi everyone, my name is Ella and I’m a Junior Biomedical Engineering major interested in upper extremity bionic prostheses. Through Medical Makers and my research efforts, I aim to design medical devices that are accessible to communities in need. Outside of school, I enjoy going to the gym, trying new places to eat, and watching the sunset with friends. Don’t hesitate to reach out with any questions!",
     image: "images/ella_meek.jpeg",
     linkedin: "https://www.linkedin.com/in/ella-meek-64a46631b",
     email: "mailto:EllaMeek2028@u.northwestern.edu"
